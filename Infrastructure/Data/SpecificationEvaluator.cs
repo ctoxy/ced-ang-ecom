1247bd5fd3f48a7968e57b779692e53c1ece5341
ctoxy <cedric.bouve@gmail.com>
2020-06-16 14:46:30 +0200

    adding pagination

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -23,6 +23,11 @@ namespace Infrastructure.Data
             {
                 query = query.OrderByDescending(spec.OrderByDescending);
             }
+            // ordre de pagination aprés le critére et l ordre important
+            if (spec.IsPagingEnabled)
+            {   
+                query = query.Skip(spec.Skip).Take(spec.Take);
+            }
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
 
             return query;
