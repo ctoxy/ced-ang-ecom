038ddbfb5be28b6a881f4c8c60ce30cc35e2781f
ctoxy <cedric.bouve@gmail.com>
2020-06-18 13:39:06 +0200

    Consuming services in the component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { IProduct } from './shared/models/product';
-import { IPagination } from './shared/models/pagination';
+
 
 @Component({
   selector: 'app-root',
@@ -10,17 +8,8 @@ import { IPagination } from './shared/models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'ecomCed';
-  products: IProduct [];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
-      console.log(response);
-      this.products = response.data;
-
-    }, error => {
-      console.log(error);
-
-    });
   }
 }
