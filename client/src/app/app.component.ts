24da67aa5b4db34cd890c51b05401fefc2cfce0b
ctoxy <cedric.bouve@gmail.com>
2020-06-18 09:26:45 +0200

    adding model

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component,OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,13 +9,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   title = 'ecomCed';
-  products:any [];
+  products: IProduct [];
 
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response:any) => {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
       console.log(response);
-      this.products= response.data;
+      this.products = response.data;
 
     }, error => {
       console.log(error);
