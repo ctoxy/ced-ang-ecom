1f55cc801ebe94cf89bb8738f321b7c32865ce80
ctoxy <cedric.bouve@gmail.com>
2020-07-01 17:02:24 +0200

    Creating the account module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ import { ServerErrorComponent } from './core/server-error/server-error.component
 
 const routes: Routes = [
   {path: '', component:HomeComponent, data: {breadcrumb: 'Home'}},
+  {path: 'account', loadChildren:() => import('./account/account.module').then(mod => mod.AccountModule)
+  , data: {skip: true}},
   {path: 'test-error', component:TestErrorComponent, data: {breadcrumb: 'Test Errors'}},
   {path: 'server-error', component:ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
   {path: 'not-found', component:NotFoundComponent, data: {breadcrumb: 'Not Found'}},
