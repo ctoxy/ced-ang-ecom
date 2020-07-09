4f9b41efe4fc34c4fd6868da5f5a2860dd34a4cf
ctoxy <cedric.bouve@gmail.com>
2020-07-09 11:01:57 +0200

    Creating the checkout components

--- client/src/app/shared/components/stepper/stepper.component.ts
+++ client/src/app/shared/components/stepper/stepper.component.ts
@@ -17,7 +17,7 @@ export class StepperComponent extends CdkStepper implements OnInit {
 
   onClick(index: number) {
     this.selectedIndex = index;
-    console.log(this.selectedIndex);
+    // console.log(this.selectedIndex);
 
   }
 
