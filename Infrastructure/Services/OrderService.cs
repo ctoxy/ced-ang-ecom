9829a71c83a23d245b75eb4e1970cc444868f935
ctoxy <cedric.bouve@gmail.com>
2020-07-22 15:30:54 +0200

    Tying the payment intent to the order

--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -11,13 +11,16 @@ namespace Infrastructure.Services
     public class OrderService : IOrderService
     {
         private readonly IBasketRepository _basketRepo;
+        private readonly IPaymentService _paymentService;
         private readonly IUnitOfWork _unitOfWork;
 
         public OrderService(IUnitOfWork unitOfWork,
-                            IBasketRepository basketRepo)
+                            IBasketRepository basketRepo,
+                            IPaymentService paymentService)
         {
             _unitOfWork = unitOfWork;            
             _basketRepo = basketRepo;
+            _paymentService = paymentService;
         }
 
         public async Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethodId, string basketId, Address shippingAddress)
@@ -38,8 +41,18 @@ namespace Infrastructure.Services
             // calc subtotal
             var subtotal = items.Sum(items => items.Price * items.Quantity);
 
+            // check to see if order exists
+            var spec = new OrderByPaymentIntentIdSpecification(basket.PaymentIntentId);
+            var existingOrder = await _unitOfWork.Repository<Order>().GetEntityWithSpec(spec);
+
+            if (existingOrder != null)
+            {
+                _unitOfWork.Repository<Order>().Delete(existingOrder);
+                await _paymentService.CreateOrUpdatePaymentIntent(basket.PaymentIntentId);
+            }
+
             //create Order
-            var order = new Order(items, buyerEmail, shippingAddress, deliveryMethod, subtotal);
+            var order = new Order(items, buyerEmail, shippingAddress, deliveryMethod, subtotal, basket.PaymentIntentId);
             _unitOfWork.Repository<Order>().Add(order);
             //  save to db
             var result = await _unitOfWork.Complete();
@@ -48,8 +61,7 @@ namespace Infrastructure.Services
             {
                 return null;
             }
-            // delete basket
-            await _basketRepo.DeleteBasketAsync(basketId);
+            
             //return order
 
             return order;
--- Infrastructure/Services/PaymentService.cs
+++ Infrastructure/Services/PaymentService.cs
@@ -31,6 +31,8 @@ namespace Infrastructure.Services
             StripeConfiguration.ApiKey = _config["StripeSettings:SecretKey"];
 
             var basket = await _basketRepository.GetBasketAsync(basketId);
+            if (basket == null) return null;
+            
             var shippingPrice = 0m;
 
             if (basket.DeliveryMethodId.HasValue)
